dcaf7ab9db042cbd4ec3e9b1d7e9b7b1c322d24e
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2025-03-13 17:34:00 +0700

    Percobaan 1 Commit 2

--- minggu5/classFaktorial.java
+++ minggu5/classFaktorial.java
@@ -2,10 +2,17 @@ package minggu5;
 public class classFaktorial{
     int faktorialBF(int n) { //  Algoritma Brute Force
         int fakto = 1;
+        int i=1;
+        while (i<=n) {
+            fakto *= i;
+            i++;
+        } return fakto;
+        /*
         for (int i=1; i<=n;i++){
             fakto = fakto * i;
         }
         return fakto;
+        */
     }
     int faktorialDC(int n) { // Algoritma Divide n Conquer
         if (n==1){
