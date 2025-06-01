eaeca801dc7ad88c1a7353878512a6e07fc22fcb
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2025-06-01 17:52:58 +0700

    Commit 1 - Percobaan 2

---
+++ Jobsheet10/P2Jobsheet10/AntrianLayanan.java
@@ -0,0 +1,81 @@
+package Jobsheet10.P2Jobsheet10;
+
+public class AntrianLayanan {
+    Mahasiswa[] data;
+    int front, rear, size, max;
+    
+    public AntrianLayanan(int max) {
+        this.max = max;
+        data = new Mahasiswa[max];
+        size = front = 0;
+        rear = -1;
+    }
+
+    public void tambahAntrian(Mahasiswa mhs) { // Enqueue
+        if (isFull()) {
+            System.out.println("Antrian penuh, tidak dapat menambah mahasiswa.");
+            return;
+        }
+        rear = (rear + 1) % max;
+        data[rear] = mhs;
+        size++;
+        System.out.println(mhs.nama + " berhasil masuk ke antrian.");
+    }
+
+    public Mahasiswa layaniMahasiswa() { // Dequeue
+        if(isEmpty()) {
+            System.out.println("Antrian kosong.");
+            return null;
+        }
+        Mahasiswa mhs = data[front];
+        front = (front + 1) % max;
+        size--;
+        return mhs;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == max;
+    }
+
+    public void lihatTerdepan() { // Peek
+        if(isEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            System.out.println("Mahasiswa terdepan: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[front].tampilkanData();
+        }
+    }
+
+    public void tampilkanSemua() { // Print
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+            return;
+        }
+        System.out.println("Daftar Mahasiswa dalam Antrian:");
+        System.out.println("NIM - NAMA - PRODI - KELAS");
+        for(int i = 0; i < size; i++) {
+            int index = (front + i) % max;
+            System.out.print((i+1) + ". ");
+            data[index].tampilkanData();
+        }
+    }
+
+    public void clear() {
+        if (!isEmpty()) {
+            front = rear = -1;
+            size = 0;
+            System.out.println("Queue berhasil dikosongkan");
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    public int getJumlahAntrian() {
+        return size;
+    }
+}
