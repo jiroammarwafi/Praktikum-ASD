3970a256859ac292d9ae8b6137526d2c9ad233ee
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2025-06-07 17:33:12 +0700

    Modifikasi

--- Jobsheet11/SLLMain14.java
+++ Jobsheet11/SLLMain14.java
@@ -1,20 +1,22 @@
 package Jobsheet11;
+import java.util.Scanner;
 
 public class SLLMain14 {
     public static void main(String[] args) {
         SingleLinkedList14 sll = new SingleLinkedList14();
-        Mahasiswa14 mhs1 = new Mahasiswa14("244107020190 (1)", "Jiro", "1E", 3.57);
-        Mahasiswa14 mhs2 = new Mahasiswa14("244107020190 (2)", "Rina", "1E", 3.56);
-        Mahasiswa14 mhs3 = new Mahasiswa14("244107020190 (3)", "Tomie", "1E", 2.56);
-        Mahasiswa14 mhs4 = new Mahasiswa14("244107020190 (4)", "Roku", "1E", 1.56);
-
-        sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Roku", mhs3);
-        sll.insertAt(2, mhs2);
-        sll.print();
+        Scanner sr = new Scanner (System.in);
+        
+        for (int i=0; i<4; i++) {
+            System.out.printf("Input Data Mahasiswa ke-%d%n", i+1);
+            System.out.print("Nama: "); String nama = sr.nextLine();
+            System.out.print("NIM: "); String nim = sr.nextLine();
+            System.out.print("Kelas: "); String kelas = sr.nextLine();
+            System.out.print("IP: "); double ipk = sr.nextDouble(); sr.nextLine();
+            Mahasiswa14 mhs = new Mahasiswa14(nim, nama, kelas, ipk); sll.addLast(mhs);
+            System.out.println("[> UPDATE");
+            sll.print();
+        }
+        System.out.println("Program Berakhir");
+        sr.close();
     }
 }
